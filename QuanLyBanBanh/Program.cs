9f97bb765e58c9d5ee24d4ca2bca8e365326125b
LEMANHHUY <habua2911@gmail.com>
2018-03-27 15:27:08 +0700

    ato5

--- QuanLyBanBanh/Program.cs
+++ QuanLyBanBanh/Program.cs
@@ -15,6 +15,7 @@ namespace QuanLyBanBanh
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmDangNhap());
+            //ato5
         }
     }
 }
